58ddbca97d5782a8c85701bb4d353ea54866ee05
Elotlan <zS20020852@estudiantes.uv.mx>
2023-10-05 15:48:32 -0600

    Se inicializa el repositorio con el fichero readme

---
+++ For.py
@@ -0,0 +1,7 @@
+cadena = 'Hola'
+#in = dentro de cada elemento
+# TODO: Nota de prueba 2
+for letra in cadena:
+    print(letra)
+else:
+    print('Fin del ciclo for')
---
+++ While.py
@@ -0,0 +1,13 @@
+"""condicion = True
+
+while condicion:
+    print('ejecutando el ciclo while')
+else:
+    print('Fin del ciclo while')
+"""
+contador = 0
+while contador < 3:
+    print(contador)
+    contador += 1;
+else:
+    print('fin del ciclo while')
